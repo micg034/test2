74013fe2512d37964b42b31a0bfc390395f93cd0
micg034 <michelle_go_3@hotmail.com>
2016-07-20 20:19:33 +0200

    Update MainController.js

--- javascripts/controllers/MainController.js
+++ javascripts/controllers/MainController.js
@@ -1,37 +1,3 @@
 app.controller('MainController', ['$scope', function($scope) {
-  $scope.gegevens = [ 
-	    /**
-	    icon: 'profile.jpg', 
-	    title: 'Persoonlijke Gegevens',
-	    name: 'Naam', 
-	    dateofbirth: '199949',
-	    nationality: 'NL',
-	    adress: 'prive',
-	    email: 'mijn@mail.com',
-	    phone: '0612345678'
-	    
-	    **/
-	    
-	    //Werkervaring
-	    {
-  	    name: 'Vrije Universiteit Amsterdam',
-  	    course: 'Lifestyle Informatics',
-  	    date: '12607 - heden',
-  	    diploma: 'Nee',
-	    },
-	    {
-  	    name: 'Vrije Universiteit Amsterdam',
-  	    course: 'Gezondheid en Leven',
-  	    date: '12456 - 12406',
-  	    diploma: 'Nee',
-	    },
-	    {
-	      name: 'Bertrand Russell College',
-	      course: 'VWO Natuur en Gezondheid',
-  	    date: '109342 - 12356',
-  	    diploma: 'Ja',
-	    }
-	    
-
-	];
+  $scope.title = 'Persoonlijke Gegevens'
 }]);
